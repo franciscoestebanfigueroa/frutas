6ec9d4606404461b10bdc96350ae49ab7379d9fa
Francisco Esteban <franciscoesteban@live.com>
2021-07-27 22:16:00 -0300

    refactoriando

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,14 @@ class MyApp extends StatelessWidget {
         ),
         body: BlocBuilder<FrutasBloc, FrutasState>(
           builder: (context, statez) {
-            return ListViewDoble();
+            return ListViewDoble(
+              itemBuilder: (BuildContext context, int index) {
+                return ItemWidget(
+                  data: datos[index],
+                  index: index,
+                );
+              },
+            );
           },
         ),
       ),
@@ -67,7 +74,9 @@ class BlocTemp extends StatelessWidget {
 }
 
 class ListViewDoble extends StatelessWidget {
-  Data datos = Data();
+  final IndexedWidgetBuilder itemBuilder;
+
+  const ListViewDoble({Key? key, required this.itemBuilder}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Transform.scale(
@@ -77,55 +86,15 @@ class ListViewDoble extends StatelessWidget {
         child: OverflowBox(
           maxHeight: MediaQuery.of(context).size.height * 1.1,
           child: GridView.builder(
-            padding: EdgeInsets.only(bottom: 200, top: 100),
-            itemCount: 20,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              childAspectRatio: 0.8,
-              crossAxisCount: 2,
-              //mainAxisSpacing: 20,
-              //  crossAxisSpacing: 20,
-            ),
-            itemBuilder: (context, index) {
-              return Transform.translate(
-                offset: Offset(0.0, index.isOdd ? 100.0 : 0.0),
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                  elevation: 11,
-                  color: Colors.white70,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ClipOval(
-                        child: Image.network(datos.url),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        'Nombre $index',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: List.generate(
-                            5,
-                            (index) => Icon(
-                                  index < 4
-                                      ? Icons.star
-                                      : Icons.star_border_outlined,
-                                  color: Colors.yellow[700],
-                                )),
-                      )
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
+              padding: EdgeInsets.only(bottom: 200, top: 100),
+              itemCount: datos.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                childAspectRatio: 0.8,
+                crossAxisCount: 2,
+                //mainAxisSpacing: 20,
+                //  crossAxisSpacing: 20,
+              ),
+              itemBuilder: itemBuilder),
         ),
       ),
     );
@@ -133,5 +102,82 @@ class ListViewDoble extends StatelessWidget {
 }
 
 class Data {
-  String get url => 'https://picsum.photos/100/100';
+  final String url;
+  final String nombre;
+  final String precio;
+
+  Data({required this.url, required this.nombre, required this.precio});
+}
+
+List<Data> datos = [
+  Data(
+      url: 'https://picsum.photos/100/100', nombre: 'Primero ', precio: '\n56'),
+  Data(
+      url: 'https://picsum.photos/100/100', nombre: 'Primero ', precio: '\n56'),
+  Data(
+      url: 'https://picsum.photos/100/100', nombre: 'Primero ', precio: '\n56'),
+  Data(
+      url: 'https://picsum.photos/100/100', nombre: 'Primero ', precio: '\n56'),
+  Data(
+      url: 'https://picsum.photos/100/100', nombre: 'Primero ', precio: '\n56'),
+  Data(
+      url: 'https://picsum.photos/100/100', nombre: 'Primero ', precio: '\n56'),
+  Data(
+      url: 'https://picsum.photos/100/100', nombre: 'Primero ', precio: '\n56'),
+  Data(
+      url: 'https://picsum.photos/100/100', nombre: 'Primero ', precio: '\n56'),
+  Data(
+      url: 'https://picsum.photos/100/100', nombre: 'Primero ', precio: '\n56'),
+  Data(url: 'https://picsum.photos/100/100', nombre: 'Primero ', precio: '\n56')
+];
+
+class ItemWidget extends StatelessWidget {
+  final index;
+  final Data data;
+
+  const ItemWidget({Key? key, required this.index, required this.data})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.translate(
+      offset: Offset(0.0, index.isOdd ? 100.0 : 0.0),
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        elevation: 11,
+        color: Colors.white70,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipOval(
+              child: Image.network(data.url),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '${data.nombre}',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              '${data.precio}',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                  5,
+                  (index) => Icon(
+                        index < 4 ? Icons.star : Icons.star_border_outlined,
+                        color: Colors.yellow[700],
+                      )),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
